ea3fbb17d5885cc0a31b7d22ee0aa787ee9b63f3
FlutLab.io <>
2021-10-04 10:51:57 +0000

    Autosave

--- lib/all_imports.dart
+++ lib/all_imports.dart
@@ -3,6 +3,9 @@ export 'package:flutter/painting.dart';
 export 'package:google_fonts/google_fonts.dart';
 export 'package:flex_color_scheme/flex_color_scheme.dart';
 
+export 'package:trdl_tool/items/home_items.dart';
+export 'package:trdl_tool/items/welcome_items.dart';
+
 export 'package:trdl_tool/home_screen.dart';
 export 'package:trdl_tool/welcome_screen.dart';
 export 'package:trdl_tool/pages/uitvoeren_plan/uitvoeren_plan.dart';
