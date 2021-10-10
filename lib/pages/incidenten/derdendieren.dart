c1adc7c80f7bff382c3491c26327990eb02768bb
FlutLab.io <>
2021-10-10 17:03:30 +0000

    Autosave

--- lib/pages/incidenten/derdendieren.dart
+++ lib/pages/incidenten/derdendieren.dart
@@ -1,7 +1,7 @@
 import 'package:trdl_tool/all_imports.dart';
 
-class HerroepenSein extends StatelessWidget {
-  const HerroepenSein({Key? key}) : super(key: key);
+class DerdenDieren extends StatelessWidget {
+  const DerdenDieren({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +29,7 @@ class HerroepenSein extends StatelessWidget {
                       Row(
                         children: [
                           Expanded(
-                            child: TitleText(
-                              title: 'Herroepen van een sein',
-                            ),
+                            child: TitleText(title: 'Derden en/of dieren langs de baan'),
                           ),
                         ],
                       ),
@@ -50,7 +48,48 @@ class HerroepenSein extends StatelessWidget {
                         children: [
                           Expanded(
                             child: BodyText(
-                              text: 'Voor bijsturing mag je een sein herroepen nadat de machinist is ingelicht of wanneer je zeker weet dat er geen machinist op of bij de trein aanwezig is.',
+                              text: 'Na de melding van onbevoegden en/of dieren op of langs het spoor met (potentieel) gevaar voor schade aan mens, materieel en/of infra, handel je als volgt:',
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBoxH(),
+                      Row(
+                        children: [
+                          SizedBoxW(),
+                          Expanded(
+                            child: BodyText(
+                              text: '- Geef een aanwijzing VR en een schouwopdracht af;',
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBoxW(),
+                          Expanded(
+                            child: BodyText(
+                              text: '- Staak het afgeven van aanwijzingen na 2 opeenvolgende nihil meldingen;',
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBoxW(),
+                          Expanded(
+                            child: BodyText(
+                              text: '- Indien de machinist meldt dat de onbevoegde of het dier zich nog steeds op of langs het spoor bevindt of er in het komende half uur geen trein rijdt, informeer je de MKS/BO;',
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBoxW(),
+                          Expanded(
+                            child: BodyText(
+                              text: '- Indien de MKS/BO is ingelicht of de melding van de MKS/BO is verkregen, staak je pas het afgeven van aanwijzingen na toestemming van de MKS/BO.',
                             ),
                           ),
                         ],
@@ -59,9 +98,7 @@ class HerroepenSein extends StatelessWidget {
                       Row(
                         children: [
                           Expanded(
-                            child: BodyText(
-                              text: 'Benader de machinist via de bestaande communicatiemogelijkheden. Als je geen contact krijgt, herroep je het sein.',
-                            ),
+                            child: BodyText(text: 'Om aangereden dieren te verwijderen, licht je MKS/BO in.'),
                           ),
                         ],
                       ),
@@ -79,7 +116,9 @@ class HerroepenSein extends StatelessWidget {
                       Row(
                         children: [
                           Expanded(
-                            child: SubTitleText(subtitle: 'Risico\'s'),
+                            child: SubTitleText(
+                              subtitle: 'Risico\'s',
+                            ),
                           ),
                         ],
                       ),
@@ -88,7 +127,7 @@ class HerroepenSein extends StatelessWidget {
                         children: [
                           Expanded(
                             child: BodyText(
-                              text: 'Het ten onrechte herroepen van seinen.',
+                              text: 'Treinen komen niet tijdig tot stilstand voor het gevaarpunt, of de snelheid van treinen wordt niet tijdig teruggebracht voor het gevaarpunt.',
                             ),
                           ),
                         ],
@@ -118,7 +157,17 @@ class HerroepenSein extends StatelessWidget {
                         children: [
                           Expanded(
                             child: BodyText(
-                              text: 'Het herroepen van een sein kan noodzakelijk zijn om een verstoring van de treindienst te voorkomen of beperken. Bij het herroepen van een sein voor bijsturing is het inlichten van de machinist wenselijk, om hem niet te laten schrikken van een wisselend seinbeeld.',
+                              text: 'Het spoor is afgesloten voor onbevoegden en/of dieren. Betreden van het spoor door onbevoegden en/of dieren kan een gevaarlijke situatie opleveren voor betrokkenen en/of de treindienst. Een machinist kan ter plaatse het risico op een gevaarlijke situatie beoordelen.',
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBoxH(),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: BodyText(
+                              text: 'De treindienstleider meldt onbevoegden en/of dieren langs het spoor zodat deze verwijderd kunnen worden en/of de afscheiding van het spoor hersteld kan worden.',
                             ),
                           ),
                         ],
