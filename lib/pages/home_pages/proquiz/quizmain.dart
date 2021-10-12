a759a5d59951a925dca233e5a2479a52dd17b90c
FlutLab.io <>
2021-10-12 17:43:50 +0000

    Autosave

--- lib/pages/home_pages/proquiz/quizmain.dart
+++ lib/pages/home_pages/proquiz/quizmain.dart
@@ -5,10 +5,98 @@ class Quizzler extends StatefulWidget {
   _QuizzlerState createState() => _QuizzlerState();
 }
 
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+}
+
 class _QuizzlerState extends State<Quizzler> {
+  List<Widget> scoreKeeper = [
+    Icon(
+      Icons.arrow_forward,
+    ),
+  ];
+
+  List<Question> questionBank = [
+    Question('Prince Harry is taller than Prince William', false),
+    Question('The star sign Aquarius is represented by a tiger', true),
+    Question('Meryl Streep has won two Academy Awards', false),
+    Question('Marrakesh is the capital of Morocco', false),
+    Question('Idina Menzel sings \'let it go\' 20 times in \'Let It Go\' from Frozen', false),
+    Question('Waterloo has the greatest number of tube platforms in London', true),
+    Question('M&M stands for Mars and Moordale', false),
+    Question('Gin is typically included in a Long Island Iced Tea', true),
+    Question('The unicorn is the national animal of Scotland', true),
+    Question('There are two parts of the body that can\t heal themselves', false),
+    Question('Howard Donald is the oldest member of Take That', true),
+    Question('The Great Wall of China is longer than the distance between London and Beijing', true),
+    Question('There are 219 episodes of Friends', false),
+    Question('\'A\' is the most common letter used in the English language', false),
+    Question('A lion\'s roar can be heard up to eight kilometres away', true),
+  ];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = questionBank[questionNumber].questionAnswer;
+    setState(() {
+      if (isFinished() == true) {
+        quizReset();
+        scoreKeeper = [
+          Icon(
+            Icons.arrow_forward_outlined,
+            color: Colors.white,
+          ),
+        ];
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          scoreKeeper.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        nextQuestion();
+      }
+    });
+  }
+
+  int questionNumber = 0;
+
+  bool isFinished() {
+    if (questionNumber >= questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void quizReset() {
+    questionNumber = 0;
+  }
+
+  void nextQuestion() {
+    if (questionNumber < questionBank.length - 1) {
+      questionNumber++;
+    } else {
+      isFinished();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Quizzler'),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -18,7 +106,7 @@ class _QuizzlerState extends State<Quizzler> {
                 flex: 5,
                 child: Center(
                   child: Text(
-                    QuizBrain.questionBank[questionNumber].questionText,
+                    questionBank[questionNumber].questionText,
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 25.0),
                   ),
@@ -39,7 +127,7 @@ class _QuizzlerState extends State<Quizzler> {
                       ),
                       onPressed: () {
                         setState(() {
-                          QuizBrain.checkAnswer(true);
+                          checkAnswer(true);
                         });
                       },
                       child: Text(
@@ -62,7 +150,7 @@ class _QuizzlerState extends State<Quizzler> {
                       ),
                       onPressed: () {
                         setState(() {
-                          QuizBrain.checkAnswer(false);
+                          checkAnswer(false);
                         });
                       },
                       child: Text(
