2a38b2dfa6c8600df3e9a1765e54adf5da4fb5c0
FlutLab.io <>
2021-11-11 18:07:17 +0000

    Autosave

--- lib/pages/achtergrondinfo/uitvoeren_plan/bijzonderheden_rijwegen/inzetten_railvoertuig.dart
+++ lib/pages/achtergrondinfo/uitvoeren_plan/bijzonderheden_rijwegen/inzetten_railvoertuig.dart
@@ -82,7 +82,7 @@ class InzettenRailvoertuigAchtergrond extends StatelessWidget {
                             ),
                           ),
                         ],
-                      ),,
+                      ),
                     ],
                   ),
                 ),
