272270be76a16c30a243288ad0bdfa286f08a574
plotsklapps <plotsklapps@gmail.com>
2022-10-19 19:02:00 +0200

    eol_end_of_file: false gezet, multiline strings gebruikt om de 80 char regel te respecteren.

--- lib/screens/achtergrond_screens/aanpassen_plan/orderacceptatie/ai_orderacceptatie.dart
+++ lib/screens/achtergrond_screens/aanpassen_plan/orderacceptatie/ai_orderacceptatie.dart
@@ -18,7 +18,7 @@ class AIOrderacceptatie extends StatelessWidget {
         title: const AppBarText(
           title: 'Achtergrondinformatie',
         ),
-        actions: [
+        actions: <Widget>[
           PopupMenuButton<WhereToGoFromAIOrderacceptatie>(
             icon: const Icon(Icons.info_outlined),
             tooltip: 'Meer informatie',
@@ -73,26 +73,26 @@ class AIOrderacceptatie extends StatelessWidget {
                     ),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Wanneer onze klanten iets willen wijzigen in de vooraf bepaalde capaciteitstoedeling of iets extra\'s willen doen zij dat middels een orderaanvraag. Reden kan zijn:',
+                      text: '''
+Wanneer onze klanten iets willen wijzigen in de vooraf bepaalde capaciteitstoedeling of iets extra's willen doen zij dat middels een orderaanvraag. Reden kan zijn:''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 1,
-                      text:
-                          '- Opheffen, wijzigen of inleggen van trein- en rangeerbewegingen;\n\n- Verzoeken om railinfracapaciteitsonttrekking voor werkzaamheden aan de infra of materieel;\n\n- Materieelwisselingen.',
+                      text: '''
+- Opheffen, wijzigen of inleggen van trein- en rangeerbewegingen;\n\n- Verzoeken om railinfracapaciteitsonttrekking voor werkzaamheden aan de infra of materieel;\n\n- Materieelwisselingen.''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
-                      text:
-                          'We kennen twee soorten orderaanvragen, lokaal en bovenlokaal.',
+                      text: '''
+We kennen twee soorten orderaanvragen, lokaal en bovenlokaal.''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
-                      text:
-                          'N.B.: In alle fasen van capaciteitsverdeling en -planning gelden de plannormen. Deze normen gelden ook voor het plannen van treinen en rangeerbewegingen in de fase van verkeersleiding. Voor bijsturing gelden deze normen niet.',
+                      text: '''
+N.B.: In alle fasen van capaciteitsverdeling en -planning gelden de plannormen. Deze normen gelden ook voor het plannen van treinen en rangeerbewegingen in de fase van verkeersleiding. Voor bijsturing gelden deze normen niet.''',
                     ),
                   ],
                 ),
@@ -111,20 +111,20 @@ class AIOrderacceptatie extends StatelessWidget {
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Lokale orderaanvragen gaan rechtstreeks naar jou en worden ingediend middels LOA Online. Indien dit systeem gestoord is (of nog niet aanwezig), dan komt de aanvraag via een formulier Lokale Orderaanvragen of per telefoon.\n\nEen LOA moet in ieder geval de volgende informatie bevatten:',
+                      text: '''
+Lokale orderaanvragen gaan rechtstreeks naar jou en worden ingediend middels LOA Online. Indien dit systeem gestoord is (of nog niet aanwezig), dan komt de aanvraag via een formulier Lokale Orderaanvragen of per telefoon.\n\nEen LOA moet in ieder geval de volgende informatie bevatten:''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 1,
                       text:
-                          '- Trein-/rangeernummer;\n\n- Plan-/vertrektijd;\n\n- \'Van\' spoor;\n\n- \'Via\' sporen (indien van toepassing);\n\n- \'Naar\' spoor;\n\n- Materieelrelatie;\n\n- Materieel- of tractiesoort;\n\n- Wel/Geen afwijken/beperkingen aan materieel gerelateerd aan infragebruik.',
+                          "- Trein-/rangeernummer;\n\n- Plan-/vertrektijd;\n\n- 'Van' spoor;\n\n- 'Via' sporen (indien van toepassing);\n\n- 'Naar' spoor;\n\n- Materieelrelatie;\n\n- Materieel- of tractiesoort;\n\n- Wel/Geen afwijken/beperkingen aan materieel gerelateerd aan infragebruik.",
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
-                      text:
-                          'De LOA moet bij voorkeur 15 minuten voor uitvoering worden ingediend, zodat je het plan tijdig kunt aanpassen en alle betrokkenen kunt informeren.\n\nDe afhandeling van lokale orderaanvragen voor goederenvervoer wijkt af van die van reizigersvervoer, tot de implementatie van LOA Online en de introductie van de maatregel \'veilig rangeren\' bij goederenvervoer.',
+                      text: """
+De LOA moet bij voorkeur 15 minuten voor uitvoering worden ingediend, zodat je het plan tijdig kunt aanpassen en alle betrokkenen kunt informeren.\n\nDe afhandeling van lokale orderaanvragen voor goederenvervoer wijkt af van die van reizigersvervoer, tot de implementatie van LOA Online en de introductie van de maatregel 'veilig rangeren' bij goederenvervoer.""",
                     ),
                     SizedBoxH(),
                     InsertImage(
@@ -150,7 +150,7 @@ class AIOrderacceptatie extends StatelessWidget {
                     BodyText(
                       indents: 0,
                       text:
-                          'Alleen geautoriseerde aanvragers van reizigersvervoerders mogen lokale orders indienen, dit doen ze via LOA Online en slechts bij uitzondering telefonisch. Je handelt LOA\'s zo snel mogelijk af. LOA\'s die meer dan 60 minuten voor uitvoering worden ingediend neem je binnen 20 minuten in behandeling. Voor het beoordelen van LOA\'s toets je de aangevraagde rangeerrijweg in SBG++.  Uitgangspunt is dat een aangevraagde rangeerrijweg voldoet aan de geldende plannormen.\n\nIn sommige gevallen resulteert het afwijzen van de lokale order op grond van de plannormen, juist tot een onveiliger situatie, bijvoorbeeld vanwege rood-seinnaderingen van andere treinen. Je mag een dergelijke order toch goedkeuren als je op basis van een bewuste afweging oordeelt dat de veiligheid daarmee beter is gediend. Een voorbeeld hiervan is een situatie waarin op een druk station een defecte trein een perronspoor bezet houdt.\n\nIndien je de aanvraag niet accepteert, geef je zo mogelijk een advies voor een alternatieve rijweg onder de optie \'opmerking\'. De aanvrager kan dan een nieuwe aanvraag indienen. Je verwerkt een goedgekeurde LOA via SBG++ in het Proces Plan Rijwegen (PPR). Een Bundel Lokale Orderaanvragen (BLOA) aangevraagd met LOA Online, verwerk je pas in het PPR als de gehele order is goedgekeurd.',
+                          '''Alleen geautoriseerde aanvragers van reizigersvervoerders mogen lokale orders indienen, dit doen ze via LOA Online en slechts bij uitzondering telefonisch. Je handelt LOA's zo snel mogelijk af. LOA's die meer dan 60 minuten voor uitvoering worden ingediend neem je binnen 20 minuten in behandeling. Voor het beoordelen van LOA's toets je de aangevraagde rangeerrijweg in SBG++.  Uitgangspunt is dat een aangevraagde rangeerrijweg voldoet aan de geldende plannormen.\n\nIn sommige gevallen resulteert het afwijzen van de lokale order op grond van de plannormen, juist tot een onveiliger situatie, bijvoorbeeld vanwege rood-seinnaderingen van andere treinen. Je mag een dergelijke order toch goedkeuren als je op basis van een bewuste afweging oordeelt dat de veiligheid daarmee beter is gediend. Een voorbeeld hiervan is een situatie waarin op een druk station een defecte trein een perronspoor bezet houdt.\n\nIndien je de aanvraag niet accepteert, geef je zo mogelijk een advies voor een alternatieve rijweg onder de optie 'opmerking'. De aanvrager kan dan een nieuwe aanvraag indienen. Je verwerkt een goedgekeurde LOA via SBG++ in het Proces Plan Rijwegen (PPR). Een Bundel Lokale Orderaanvragen (BLOA) aangevraagd met LOA Online, verwerk je pas in het PPR als de gehele order is goedgekeurd.''',
                     ),
                   ],
                 ),
@@ -170,13 +170,13 @@ class AIOrderacceptatie extends StatelessWidget {
                     BodyText(
                       indents: 0,
                       text:
-                          'Goederenvervoerders dienen lokale orders volgens de plaatselijk afgesproken werkwijze in:',
+                          '''Goederenvervoerders dienen lokale orders volgens de plaatselijk afgesproken werkwijze in:''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 1,
-                      text:
-                          '- Voor het beoordelen van deze lokale orders, toets je deze op beschikbaarheid en verdringing;\n\n- Indien je de aanvraag niet accepteert, meld je dit aan de aanvrager. Deze kan dan een nieuwe aanvraag indienen;\n\n- Je verwerkt een goedgekeurd LOA via SBG++ in het ProcesPlan Rijwegen (PPR);\n\n- Je licht de belanghebbenden in over de planaanpassingen.',
+                      text: '''
+- Voor het beoordelen van deze lokale orders, toets je deze op beschikbaarheid en verdringing;\n\n- Indien je de aanvraag niet accepteert, meld je dit aan de aanvrager. Deze kan dan een nieuwe aanvraag indienen;\n\n- Je verwerkt een goedgekeurd LOA via SBG++ in het ProcesPlan Rijwegen (PPR);\n\n- Je licht de belanghebbenden in over de planaanpassingen.''',
                     ),
                   ],
                 ),
@@ -207,7 +207,7 @@ class AIOrderacceptatie extends StatelessWidget {
                     BodyText(
                       indents: 0,
                       text:
-                          'Indien je de aanvraag niet accepteert, geef je zo mogelijk een advies voor een alternatieve rijweg onder de optie \'opmerking\'. De aanvrager kan dan een nieuwe aanvraag indienen. Je verwerkt een goedgekeurde LOA via SBG++ in het Proces Plan Rijwegen (PPR). Een Bundel Lokale Orderaanvragen (BLOA) aangevraagd met LOA Online, verwerk je pas in het PPR als de gehele order is goedgekeurd.',
+                          '''Indien je de aanvraag niet accepteert, geef je zo mogelijk een advies voor een alternatieve rijweg onder de optie 'opmerking'. De aanvrager kan dan een nieuwe aanvraag indienen. Je verwerkt een goedgekeurde LOA via SBG++ in het Proces Plan Rijwegen (PPR). Een Bundel Lokale Orderaanvragen (BLOA) aangevraagd met LOA Online, verwerk je pas in het PPR als de gehele order is goedgekeurd.''',
                     ),
                   ],
                 ),
@@ -227,19 +227,19 @@ class AIOrderacceptatie extends StatelessWidget {
                     BodyText(
                       indents: 0,
                       text:
-                          'Bovenlokale orderaanvragen worden ingediend bij de DVL. De DVL toetst deze concept dienstregeling vooraf bij de TRDL (o.a. op lokale verdringing). Om deze toets juist te kunnen uitvoeren, moet de TRDL op de hoogte zijn van de voor hem relevante gegevens. Wat moet de TRDL weten:',
+                          '''Bovenlokale orderaanvragen worden ingediend bij de DVL. De DVL toetst deze concept dienstregeling vooraf bij de TRDL (o.a. op lokale verdringing). Om deze toets juist te kunnen uitvoeren, moet de TRDL op de hoogte zijn van de voor hem relevante gegevens. Wat moet de TRDL weten:''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 1,
                       text:
-                          '- Treinnummer;\n\n- Begin- en eindpunt;\n\n- De te volgen route (volgens welke dienstregelingspunten);\n\n- Tijden (aankomst/vertrek of doorrijden);\n\n- Treinlengte;\n\n- Materieel of tractiesoort;\n\n- Eventuele bijzonderheden infra-gebruik.',
+                          ''''- Treinnummer;\n\n- Begin- en eindpunt;\n\n- De te volgen route (volgens welke dienstregelingspunten);\n\n- Tijden (aankomst/vertrek of doorrijden);\n\n- Treinlengte;\n\n- Materieel of tractiesoort;\n\n- Eventuele bijzonderheden infra-gebruik.''',
                     ),
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
                       text:
-                          'Je toets de rijweg op verdringing en meldt het resultaat aan de DVL.',
+                          '''Je toets de rijweg op verdringing en meldt het resultaat aan de DVL.''',
                     ),
                   ],
                 ),
@@ -258,8 +258,8 @@ class AIOrderacceptatie extends StatelessWidget {
                     SizedBoxH(),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Er wordt een onderscheid gemaakt tussen plannen en besturen in de fase van Verkeersleiding. Plannen behelst het toevoegen van nieuwe dienstregelingstreinen (bovenlokaal) en rangeerbewegingen (lokaal) aan het actuele plan.\n\nBesturen behelst het muteren van reeds geplande dienstregelingstreinen en reeds geplande rangeerbewegingen, vaak naar aanleiding van incidenten op het spoor. Voor de planning gelden de plannormen, voor de besturing is dat (nog) niet altijd mogelijk.',
+                      text: '''
+Er wordt een onderscheid gemaakt tussen plannen en besturen in de fase van Verkeersleiding. Plannen behelst het toevoegen van nieuwe dienstregelingstreinen (bovenlokaal) en rangeerbewegingen (lokaal) aan het actuele plan.\n\nBesturen behelst het muteren van reeds geplande dienstregelingstreinen en reeds geplande rangeerbewegingen, vaak naar aanleiding van incidenten op het spoor. Voor de planning gelden de plannormen, voor de besturing is dat (nog) niet altijd mogelijk.''',
                     ),
                     SizedBoxH(),
                     BoldText(
@@ -269,8 +269,8 @@ class AIOrderacceptatie extends StatelessWidget {
                     ),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Voor het plannen van goederentreinen in de fase van Verkeersleiding zijn in de dienstregeling lege treinpaden (BUP-paden) opgenomen. Dit zijn vooraf vastgelegde paden in de bestaande dienstregeling waarin treinen kunnen worden ingelegd. Bijv. voor het plannen van conflictvrije rangeerbewegingen op het emplacement. Zijn deze standaard rangeerrijwegen eenmaal bepaald (\'Rijwegencatalogus\'), dan moeten deze worden toegepast bij de afhandeling van lokale orders.\n\nNiet alle rangeerbewegingen kunnen worden gepland in vooraf gedefinieerde rangeerrijwegen. Deze rangeerrijwegen zullen in de \'witte ruimte\' van het plan gepland worden. Om de TRDL in staat te stellen ook deze rijwegen volgens de plannormen te plannen is SBG++ voorzien van conflictsignalering. Zo kan je concept rangeerrijwegen aanmaken en deze toetsen op mogelijke conflicten.',
+                      text: '''
+Voor het plannen van goederentreinen in de fase van Verkeersleiding zijn in de dienstregeling lege treinpaden (BUP-paden) opgenomen. Dit zijn vooraf vastgelegde paden in de bestaande dienstregeling waarin treinen kunnen worden ingelegd. Bijv. voor het plannen van conflictvrije rangeerbewegingen op het emplacement. Zijn deze standaard rangeerrijwegen eenmaal bepaald ('Rijwegencatalogus'), dan moeten deze worden toegepast bij de afhandeling van lokale orders.\n\nNiet alle rangeerbewegingen kunnen worden gepland in vooraf gedefinieerde rangeerrijwegen. Deze rangeerrijwegen zullen in de 'witte ruimte' van het plan gepland worden. Om de TRDL in staat te stellen ook deze rijwegen volgens de plannormen te plannen is SBG++ voorzien van conflictsignalering. Zo kan je concept rangeerrijwegen aanmaken en deze toetsen op mogelijke conflicten.''',
                     ),
                   ],
                 ),
@@ -288,8 +288,8 @@ class AIOrderacceptatie extends StatelessWidget {
                     ),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Voor het inleggen van nieuwe treinbewegingen en verleggen van bestaande treinbewegingen zijn normen vastgelegd. Hieronder staan tabellen, die de diverse normen weergeven:',
+                      text: '''
+Voor het inleggen van nieuwe treinbewegingen en verleggen van bestaande treinbewegingen zijn normen vastgelegd. Hieronder staan tabellen, die de diverse normen weergeven:''',
                     ),
                     SizedBoxH(),
                     InsertImage(
@@ -309,8 +309,8 @@ class AIOrderacceptatie extends StatelessWidget {
                     ),
                     BodyText(
                       indents: 0,
-                      text:
-                          'De technisch minimale norm voor halterende goederentreinen is 2 minuten. Voor reizigerstreinen geldt een minimale norm van < 1minuut, in de planning (is korte stop +).',
+                      text: '''
+De technisch minimale norm voor halterende goederentreinen is 2 minuten. Voor reizigerstreinen geldt een minimale norm van < 1minuut, in de planning (is korte stop +).''',
                     ),
                     SizedBoxH(),
                     BoldText(
@@ -319,8 +319,8 @@ class AIOrderacceptatie extends StatelessWidget {
                     ),
                     BodyText(
                       indents: 0,
-                      text:
-                          'Voor het kopmaken en keren geldt een minimumnorm van 4 minuten.',
+                      text: '''
+Voor het kopmaken en keren geldt een minimumnorm van 4 minuten.''',
                     ),
                     SizedBoxH(),
                     BoldText(
