2678dc063476c134cd6ea955a767e68e83120043
plotsklapps <plotsklapps@gmail.com>
2022-10-27 18:38:41 +0000

    Still refactoring... yaaawn

--- lib/screens/werkwijze_screens/incidenten/materieel/atb_veiligheidsstoring/ww_atb_veiligheidsstoring.dart
+++ lib/screens/werkwijze_screens/incidenten/materieel/atb_veiligheidsstoring/ww_atb_veiligheidsstoring.dart
@@ -16,112 +16,100 @@ class WWAtbVeiligheidsstoring extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const AppBarText(
-          title: 'Werkwijze',
+          title: Utils.appBarTitleWW,
         ),
-        actions: <Widget>[
-          PopupMenuButton<WhereToGoFromWWAtbVeiligheidsstoring>(
-            icon: const Icon(Utils.iconInfo),
-            tooltip: 'Meer informatie',
-            onSelected: (WhereToGoFromWWAtbVeiligheidsstoring result) async {
-              if (result == WhereToGoFromWWAtbVeiligheidsstoring.home_screen) {
-                await Navigator.pushNamed(context, 'home_screen');
-              } else if (result ==
-                  WhereToGoFromWWAtbVeiligheidsstoring.ai_atb) {
-                await Navigator.pushNamed(context, 'ai_atb');
-              } else {
-                Navigator.pop(context);
-              }
-            },
-            itemBuilder: (BuildContext context) =>
-                <PopupMenuEntry<WhereToGoFromWWAtbVeiligheidsstoring>>[
-              const PopupMenuItem<WhereToGoFromWWAtbVeiligheidsstoring>(
-                value: WhereToGoFromWWAtbVeiligheidsstoring.home_screen,
-                child: MenuItemContent(
-                  icon: Utils.iconHome,
-                  text: 'Home',
-                ),
+        actions: const <Widget>[
+          WWATBVeiligheidsstoring(),
+          HomeButton(),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: const <TextCard>[
+              TextCard(
+                widgetList: <Widget>[
+                  TitleText(
+                    title: 'ATB veiligheidsstoring',
+                  ),
+                  SizedBoxH(),
+                  SubTitleText(
+                    subtitle: Strings.procedure,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text:
+                        'Bij een ATB veiligheidsstoring aan de baan voorkom je rijweginstelling naar het betrokken spoor.\n\nJe stelt pas een rijweg in naar het betrokken spoor nadat je de MCN hebt ingelicht.',
+                  ),
+                ],
               ),
-              const PopupMenuItem<WhereToGoFromWWAtbVeiligheidsstoring>(
-                value: WhereToGoFromWWAtbVeiligheidsstoring.ai_atb,
-                child: MenuItemContent(
-                  icon: Utils.iconAI,
-                  text: 'AI ATB',
-                ),
+              TextCard(
+                widgetList: <Widget>[
+                  SubTitleText(
+                    subtitle: Strings.risico,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text: 'Het opvolgen van het onjuiste seinbeeld door MCN.',
+                  ),
+                ],
+              ),
+              TextCard(
+                widgetList: <Widget>[
+                  SubTitleText(
+                    subtitle: Strings.context,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text:
+                        'Er is sprake van een ATB veiligheidsstoring wanneer de treinbeveiliging (ATB) een hogere snelheid aangeeft dan de blokbeveiliging (seinen). De MCN is mogelijk niet op de hoogte van de afwijking en wordt niet door de treinbeveiliging beschermd.',
+                  ),
+                ],
               ),
             ],
           ),
-          const HomeButton(),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Card>[
-            /*PROCEDURE CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    TitleText(
-                      title: 'ATB veiligheidsstoring',
-                    ),
-                    SizedBoxH(),
-                    SubTitleText(
-                      subtitle: Strings.procedure,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text:
-                          'Bij een ATB veiligheidsstoring aan de baan voorkom je rijweginstelling naar het betrokken spoor.\n\nJe stelt pas een rijweg in naar het betrokken spoor nadat je de MCN hebt ingelicht.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            /*RISICO CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    SubTitleText(
-                      subtitle: Strings.risico,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text: 'Het opvolgen van het onjuiste seinbeeld door MCN.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            /*CONTEXT CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    SubTitleText(
-                      subtitle: Strings.context,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text:
-                          'Er is sprake van een ATB veiligheidsstoring wanneer de treinbeveiliging (ATB) een hogere snelheid aangeeft dan de blokbeveiliging (seinen). De MCN is mogelijk niet op de hoogte van de afwijking en wordt niet door de treinbeveiliging beschermd.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
         ),
       ),
+    );
+  }
+}
+
+class WWATBVeiligheidsstoring extends StatelessWidget {
+  const WWATBVeiligheidsstoring({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<WhereToGoFromWWAtbVeiligheidsstoring>(
+      icon: const Icon(Utils.iconInfo),
+      tooltip: 'Meer informatie',
+      onSelected: (WhereToGoFromWWAtbVeiligheidsstoring result) async {
+        if (result == WhereToGoFromWWAtbVeiligheidsstoring.home_screen) {
+          await Navigator.pushNamed(context, 'home_screen');
+        } else if (result == WhereToGoFromWWAtbVeiligheidsstoring.ai_atb) {
+          await Navigator.pushNamed(context, 'ai_atb');
+        } else {
+          Navigator.pop(context);
+        }
+      },
+      itemBuilder: (BuildContext context) =>
+          <PopupMenuEntry<WhereToGoFromWWAtbVeiligheidsstoring>>[
+        const PopupMenuItem<WhereToGoFromWWAtbVeiligheidsstoring>(
+          value: WhereToGoFromWWAtbVeiligheidsstoring.home_screen,
+          child: MenuItemContent(
+            icon: Utils.iconHome,
+            text: 'Home',
+          ),
+        ),
+        const PopupMenuItem<WhereToGoFromWWAtbVeiligheidsstoring>(
+          value: WhereToGoFromWWAtbVeiligheidsstoring.ai_atb,
+          child: MenuItemContent(
+            icon: Utils.iconAI,
+            text: 'AI ATB',
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/werkwijze_screens/incidenten/materieel/gevaarlijke_stoffen/ww_gevaarlijke_stoffen2.dart
+++ lib/screens/werkwijze_screens/incidenten/materieel/gevaarlijke_stoffen/ww_gevaarlijke_stoffen2.dart
@@ -16,142 +16,130 @@ class WWGevaarlijkeStoffen2 extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const AppBarText(
-          title: 'Werkwijze',
+          title: Utils.appBarTitleWW,
         ),
-        actions: <Widget>[
-          PopupMenuButton<WhereToGoFromWWGevaarlijkeStoffen2>(
-            icon: const Icon(Utils.iconInfo),
-            tooltip: 'Meer informatie',
-            onSelected: (WhereToGoFromWWGevaarlijkeStoffen2 result) async {
-              if (result == WhereToGoFromWWGevaarlijkeStoffen2.home_screen) {
-                await Navigator.pushNamed(context, 'home_screen');
-              } else if (result ==
-                  WhereToGoFromWWGevaarlijkeStoffen2.ai_gevaarlijke_stoffen) {
-                await Navigator.pushNamed(context, 'ai_gevaarlijke_stoffen');
-              } else {
-                Navigator.pop(context);
-              }
-            },
-            itemBuilder: (BuildContext context) =>
-                <PopupMenuEntry<WhereToGoFromWWGevaarlijkeStoffen2>>[
-              const PopupMenuItem<WhereToGoFromWWGevaarlijkeStoffen2>(
-                value: WhereToGoFromWWGevaarlijkeStoffen2.home_screen,
-                child: MenuItemContent(
-                  icon: Utils.iconHome,
-                  text: 'Home',
-                ),
+        actions: const <Widget>[
+          WWGevaarlijkeStoffen2Navigation(),
+          HomeButton(),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: const <TextCard>[
+              TextCard(
+                widgetList: <Widget>[
+                  TitleText(
+                    title: 'Gevaarlijke stoffen',
+                  ),
+                  SizedBoxH(),
+                  SubTitleText(
+                    subtitle: Strings.procedure,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text:
+                        'Als je een onregelmatigheid met gevaarlijke stoffen gemeld krijgt, neem je de volgende maatregelen:',
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 1,
+                    text:
+                        '- Staak de trein- en rangeerdienst op het betrokken gebied;\n\n- Voorkom rijweginstelling naar het betrokken gebied;\n\n- Laat de MKS/BO de wisselverwarming doven.',
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text: 'Jouw melding bevat in ieder geval:',
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 1,
+                    text:
+                        '- De plaats van de onregelmatigheid;\n\n- De betrokken trein;\n\n- De aard van de onregelmatigheid.',
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text: 'En indien mogelijk:',
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 1,
+                    text:
+                        '- GEVI-nummer;\n\n- UN-nummer;\n\n- Gevaaretiket;\n\n- Plaats van de wagen in de trein;\n\n- Wagennummer.',
+                  ),
+                ],
               ),
-              const PopupMenuItem<WhereToGoFromWWGevaarlijkeStoffen2>(
-                value:
-                    WhereToGoFromWWGevaarlijkeStoffen2.ai_gevaarlijke_stoffen,
-                child: MenuItemContent(
-                  icon: Utils.iconAI,
-                  text: 'AI Gevaarlijke Stoffen',
-                ),
+              TextCard(
+                widgetList: <Widget>[
+                  SubTitleText(
+                    subtitle: Strings.risico,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text:
+                        'Treinen komen niet tijdig tot stilstand voor het gevaarpunt, of de snelheid van treinen wordt niet tijdig teruggebracht voor het gevaarpunt.',
+                  ),
+                ],
+              ),
+              TextCard(
+                widgetList: <Widget>[
+                  SubTitleText(
+                    subtitle: Strings.context,
+                  ),
+                  SizedBoxH(),
+                  BodyText(
+                    indents: 0,
+                    text:
+                        'Een trein waarbij een incident met gevaarlijke stoffen optreedt mag niet verder rijden. Afhankelijk van de gevaarlijke stof moet de trein- en rangeerdienst gestaakt worden. De hulpdiensten bepalen op basis van windrichting, locatie, gevaarlijke stof en/of grootte van uitstroom hun inzet.',
+                  ),
+                ],
               ),
             ],
           ),
-          const HomeButton(),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Card>[
-            /*PROCEDURE CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    TitleText(
-                      title: 'Gevaarlijke stoffen',
-                    ),
-                    SizedBoxH(),
-                    SubTitleText(
-                      subtitle: Strings.procedure,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text:
-                          'Als je een onregelmatigheid met gevaarlijke stoffen gemeld krijgt, neem je de volgende maatregelen:',
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 1,
-                      text:
-                          '- Staak de trein- en rangeerdienst op het betrokken gebied;\n\n- Voorkom rijweginstelling naar het betrokken gebied;\n\n- Laat de MKS/BO de wisselverwarming doven.',
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text: 'Jouw melding bevat in ieder geval:',
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 1,
-                      text:
-                          '- De plaats van de onregelmatigheid;\n\n- De betrokken trein;\n\n- De aard van de onregelmatigheid.',
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text: 'En indien mogelijk:',
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 1,
-                      text:
-                          '- GEVI-nummer;\n\n- UN-nummer;\n\n- Gevaaretiket;\n\n- Plaats van de wagen in de trein;\n\n- Wagennummer.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            /*RISICO CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    SubTitleText(
-                      subtitle: Strings.risico,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text:
-                          'Treinen komen niet tijdig tot stilstand voor het gevaarpunt, of de snelheid van treinen wordt niet tijdig teruggebracht voor het gevaarpunt.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            /*CONTEXT CARD*/
-            Card(
-              elevation: Utils.kCardElevation,
-              child: Padding(
-                padding: Utils.kCardPadding,
-                child: Column(
-                  children: const <Widget>[
-                    SubTitleText(
-                      subtitle: Strings.context,
-                    ),
-                    SizedBoxH(),
-                    BodyText(
-                      indents: 0,
-                      text:
-                          'Een trein waarbij een incident met gevaarlijke stoffen optreedt mag niet verder rijden. Afhankelijk van de gevaarlijke stof moet de trein- en rangeerdienst gestaakt worden. De hulpdiensten bepalen op basis van windrichting, locatie, gevaarlijke stof en/of grootte van uitstroom hun inzet.',
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
         ),
       ),
+    );
+  }
+}
+
+class WWGevaarlijkeStoffen2Navigation extends StatelessWidget {
+  const WWGevaarlijkeStoffen2Navigation({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<WhereToGoFromWWGevaarlijkeStoffen2>(
+      icon: const Icon(Utils.iconInfo),
+      tooltip: 'Meer informatie',
+      onSelected: (WhereToGoFromWWGevaarlijkeStoffen2 result) async {
+        if (result == WhereToGoFromWWGevaarlijkeStoffen2.home_screen) {
+          await Navigator.pushNamed(context, 'home_screen');
+        } else if (result ==
+            WhereToGoFromWWGevaarlijkeStoffen2.ai_gevaarlijke_stoffen) {
+          await Navigator.pushNamed(context, 'ai_gevaarlijke_stoffen');
+        } else {
+          Navigator.pop(context);
+        }
+      },
+      itemBuilder: (BuildContext context) =>
+          <PopupMenuEntry<WhereToGoFromWWGevaarlijkeStoffen2>>[
+        const PopupMenuItem<WhereToGoFromWWGevaarlijkeStoffen2>(
+          value: WhereToGoFromWWGevaarlijkeStoffen2.home_screen,
+          child: MenuItemContent(
+            icon: Utils.iconHome,
+            text: 'Home',
+          ),
+        ),
+        const PopupMenuItem<WhereToGoFromWWGevaarlijkeStoffen2>(
+          value: WhereToGoFromWWGevaarlijkeStoffen2.ai_gevaarlijke_stoffen,
+          child: MenuItemContent(
+            icon: Utils.iconAI,
+            text: 'AI Gevaarlijke Stoffen',
+          ),
+        ),
+      ],
     );
   }
 }
