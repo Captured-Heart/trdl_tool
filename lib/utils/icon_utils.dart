867cc6aec95eb7cb01fe976a65b633ff756c0ab2
plotsklapps <plotsklapps@gmail.com>
2022-11-09 19:21:56 +0100

    Added pictures to ai_inzet_railvoertuig_icb.dart + info. Refactored IconUtils class -> Been doing it wrong for a looong time!

--- lib/utils/icon_utils.dart
+++ lib/utils/icon_utils.dart
@@ -1,14 +1,10 @@
 import '/all_imports.dart';
 
 class IconUtils {
-  static const IconData iconHome =
-      IconData(0xf107, fontFamily: 'MaterialIcons');
-  static const IconData iconInfo =
-      IconData(0xf128, fontFamily: 'MaterialIcons');
-  static const IconData iconWW = IconData(0xf458, fontFamily: 'MaterialIcons');
-  static const IconData iconAI = IconData(0xf1c2, fontFamily: 'MaterialIcons');
-  static const IconData iconGame =
-      IconData(0xf3ca, fontFamily: 'MaterialIcons');
-  static const IconData iconChat =
-      IconData(0xf0b0, fontFamily: 'MaterialIcons');
+  static const IconData iconHome = Icons.home_outlined;
+  static const IconData iconInfo = Icons.info_outline;
+  static const IconData iconWW = Icons.train_outlined;
+  static const IconData iconAI = Icons.menu_book_outlined;
+  static const IconData iconGame = Icons.sports_esports_outlined;
+  static const IconData iconChat = Icons.forum_outlined;
 }
